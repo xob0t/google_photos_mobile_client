c18bc2c16a328a663feb5471e1c3af5677adb9bf
xob0t <5348245@gmail.com>
2024-12-26 08:42:51 +0300

    chore: version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gphotos_mobile_client",
-    version="0.2.0",
+    version="0.3.0",
     description="Reverse engineered Google Photos mobile API client",
     author="xob0t",
     url="https://github.com/xob0t/gphotos_mobile_client",
