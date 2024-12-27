3801b5f6b7f600a404c816a0a62e57cf2bc0f270
xob0t <5348245@gmail.com>
2024-12-27 21:30:47 +0300

    chore(tests): mkv upload test

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -5,16 +5,21 @@ from gphotos_mobile_client import GPhotosMobileClient
 
 class TestUpload(unittest.TestCase):
     def setUp(self):
-        self.file_path = "media/image.png"
-        self.sha1_hash_b64 = "bjvmULLYvkVj8jWVQFu1Pl98hYA="
+        self.image_file_path = "media/image.png"
+        self.image_sha1_hash_b64 = "bjvmULLYvkVj8jWVQFu1Pl98hYA="
+        self.mkv_file_path = "media/sample_640x360.mkv"
         self.client = GPhotosMobileClient()
 
-    def test_upload(self):
-        media_key = self.client.upload(target=self.file_path, force_upload=True, show_progress=True)
+    def test_image_upload(self):
+        media_key = self.client.upload(target=self.image_file_path, force_upload=True, show_progress=True)
+        print(media_key)
+
+    def test_mkv_upload(self):
+        media_key = self.client.upload(target=self.mkv_file_path, force_upload=True, show_progress=True)
         print(media_key)
 
     def test_hash_check(self):
-        if media_key := self.client.check_hash(self.sha1_hash_b64):
+        if media_key := self.client.check_hash(self.image_sha1_hash_b64):
             print(media_key)
         else:
             print("No remote media with matching hash found.")
